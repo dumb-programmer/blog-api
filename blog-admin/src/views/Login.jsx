f5685a0113faf586db8a4ed886d90bdef8ee5a85
Asad Khan <asadkhan6192@gmail.com>
2023-07-06 12:51:00 +0500

    Add Error Handling to login form

--- blog-admin/src/views/Login.jsx
+++ blog-admin/src/views/Login.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import login from "../api/login";
-import "../styles/Form.css";
-import useAuthContext from "../hooks/useAuthContext";
 import { useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
+import login from "../api/login";
+import useAuthContext from "../hooks/useAuthContext";
+import "../styles/Form.css";
 
 const Login = () => {
   const [data, setData] = useState({ email: "", password: "" });
-  const { setUser } = useAuthContext();
+  const [errors, setErrors] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { setUser, setToken } = useAuthContext();
   const navigate = useNavigate();
 
   const handleInput = (e) => {
@@ -14,24 +17,49 @@ const Login = () => {
       ...data,
       [e.target.name]: e.target.value,
     });
+    setErrors(null);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    setErrors(null);
     try {
       const response = await login(data);
-      const { token } = await response.json();
-      localStorage.setItem("token", token);
-      setUser(token);
-      navigate("/");
+      if (response.status === 200) {
+        const { token, user } = await response.json();
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", user);
+        setUser(user);
+        setToken(token);
+        navigate("/");
+      } else if (response.status === 404) {
+        setErrors({
+          email: "No user with this email exists",
+        });
+      } else if (response.status === 401) {
+        setErrors({
+          password: "Incorrect password",
+        });
+      } else if (response.status === 403) {
+        const { errors: errorMessage } = await response.json();
+        setErrors({
+          [errorMessage.path]: errorMessage.msg,
+        });
+      } else if (response.status === 429) {
+        setErrors({
+          tooManyRequests: "There were too many requests, calm down",
+        });
+      }
     } catch (error) {
       console.error(error);
     }
+    setLoading(false);
   };
 
   return (
     <main className="centered">
-      <form className="form" onSubmit={handleSubmit}>
+      <form className="form" onSubmit={loading ? null : handleSubmit}>
         <h1>Login</h1>
         <div className="form-control">
           <label htmlFor="email">Email</label>
@@ -39,9 +67,11 @@ const Login = () => {
             id="email"
             type="email"
             name="email"
+            className={errors?.email ? "input__invalid" : ""}
             onInput={handleInput}
             required
           />
+          <span className="error-message">{errors?.email}</span>
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
@@ -49,12 +79,17 @@ const Login = () => {
             id="password"
             type="password"
             name="password"
+            className={errors?.password ? "input__invalid" : ""}
             onInput={handleInput}
             required
           />
+          <span className="error-message">{errors?.password}</span>
         </div>
+        <span className="error-message">{errors?.tooManyRequests}</span>
         <div className="form-control flex-end">
-          <button className="btn primary-btn">Login</button>
+          <button className="btn primary-btn" disabled={loading}>
+            {loading ? <Loader size={24} color="#fff" /> : "Login"}
+          </button>
         </div>
       </form>
     </main>
