bc7e586cf16c85fee19b154f02bb23b00cf1e3bc
Asad Khan <asadkhan6192@gmail.com>
2023-08-02 12:21:42 +0500

    Fix CORS configuration

--- server/middlewares/corsClientOrAdmin.js
+++ server/middlewares/corsClientOrAdmin.js
@@ -1,6 +1,12 @@
 const cors = require("cors");
 
-const origin = process.env.NODE_ENV === "development" ? ["http://localhost:5173", "http://localhost:5174"] : [process.env.CLIENT_ORIGIN, process.env.ADMIN_ORIGIN];
+const allowList = process.env.NODE_ENV === "development" ? ["http://localhost:5173", "http://localhost:5174"] : [process.env.CLIENT_ORIGIN, process.env.ADMIN_ORIGIN];
+const origin = (origin, cb) => {
+    if (allowList.indexOf(origin) !== -1) {
+        return cb(null, true);
+    }
+    return cb(null, false);
+}
 const corsClientOrAdmin = () => cors({ origin });
 
 module.exports = corsClientOrAdmin;
